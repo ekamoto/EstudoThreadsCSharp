222575954e9bc23c097e566053b61a540b61e63e
Leandro Shindi Ekamoto <leandro_shindi@hotmail.com>
2019-11-26 19:36:43 -0300

    Adicionando barra de progresso

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -40,21 +40,34 @@ namespace ByteBank.View
 
             var contas = r_Repositorio.GetContaClientes();
            
-            AtualizarView(new List<string>(), TimeSpan.Zero);
+            PgsProgresso.Maximum = contas.Count();
+
+            LimparView();
 
             var inicio = DateTime.Now;
 
-            var resultado = await ConsolidarContas(contas);
+            var progress = new Progress<String>(str => PgsProgresso.Value++);
+            // var byteBankProgress = new ByteBankProgress<String>(str =>
+            // PgsProgresso.Value++);
+
+            var resultado = await ConsolidarContas(contas, progress);
 
             var fim = DateTime.Now;
             AtualizarView(resultado, fim - inicio);
             BtnProcessar.IsEnabled = true;
         }
 
-        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso)
         {
             var tasks = contas.Select(conta =>
-                Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(conta))
+                Task.Factory.StartNew(() =>
+                {
+                    var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta);
+
+                    reportadorDeProgresso.Report(resultadoConsolidacao);
+
+                    return resultadoConsolidacao;
+                })
             );
 
             return await Task.WhenAll(tasks);
@@ -67,6 +80,13 @@ namespace ByteBank.View
 
             LstResultados.ItemsSource = result;
             TxtTempo.Text = mensagem;
+        }
+
+        private void LimparView()
+        {
+            LstResultados.ItemsSource = null;
+            TxtTempo.Text = null;
+            PgsProgresso.Value = 0;
         }
     }
 }
